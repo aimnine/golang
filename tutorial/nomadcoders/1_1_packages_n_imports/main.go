88bea59d7ad9e0ec3453afefce44ebfbdf951c7b
aimnine <jojoon9@kaist.ac.kr>
2020-04-28 07:10:52 +0000

    completed to 1_1

---
+++ tutorial/nomadcoders/1_1_packages_n_imports/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"aimnine/golang/tutorial/nomadcoders/1_1_packages_n_imports/something"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	something.SayHello()
+	//	something.sayBye()
+}
