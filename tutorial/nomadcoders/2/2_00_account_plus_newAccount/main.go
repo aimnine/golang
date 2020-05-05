8563a6b590870efb7ec7c0ae4c599c132b9f80f8
aimnine <jojoon9@kaist.ac.kr>
2020-05-05 10:36:21 +0000

    2-00

--- tutorial/nomadcoders/2/2_00_account_plus_newAccount/main.go
+++ tutorial/nomadcoders/2/2_00_account_plus_newAccount/main.go
@@ -1,1 +1,9 @@
 package main
+
+import "fmt"
+import "github.com/aimnine/golang/tutorial/nomadcoders/2/2_00_account_plus_newAccount/accounts"
+
+func main(){
+	account := accounts.NewAccount("aimine")
+	fmt.Println(account)
+}
