b335a966631de1db0fc679b6392c1bf2a6c97bc7
aimnine <jojoon9@kaist.ac.kr>
2020-05-06 00:35:39 +0000

    test

--- tutorial/nomadcoders/2/2_00_account_plus_newAccount/main.go
+++ tutorial/nomadcoders/2/2_00_account_plus_newAccount/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/aimnine/golang/tutorial/nomadcoders/2/2_00_account_plus_newAccount/test"
+
 	"github.com/aimnine/golang/tutorial/nomadcoders/2/2_00_account_plus_newAccount/accounts"
+	"github.com/aimnine/golang/tutorial/nomadcoders/2/2_00_account_plus_newAccount/test"
 )
 
-func main(){
+func main() {
 	account := accounts.NewAccount("aimine")
 	fmt.Println(account)
 	t := test.Test("asdf")
