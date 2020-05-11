43b1a9df32ee9f2c948dd9c1876b91f4c6c991a7
aimnine <jojoon9@kaist.ac.kr>
2020-05-11 09:22:16 +0000

    from 3.0 to 3.1

---
+++ tutorial/nomadcoders/3/3_01_slow_urlChecker/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request Failed")
+
+func main() {
+	// var results = map[string]string{}
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	// results["hello"] = "Hello"
+
+	for _, url := range urls {
+		result := "OK"
+		// fmt.Println(url)
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	// fmt.Println(results)
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	fmt.Println(err)
+	return nil
+}
